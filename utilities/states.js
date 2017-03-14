123c81fcff889a38f2d326e5d73fd068fcb885ab
Neo Genesis <genesis_neo@live.com>
2017-03-14 12:53:02 +0400

    added states.js for changing preview page state

---
+++ utilities/states.js
@@ -0,0 +1,55 @@
+
+window.onload = function(){
+
+    function hasClass(el, name) {
+        return new RegExp('(\\s|^)'+name+'(\\s|$)').test(el.className);
+    }
+    function addClass(el, name) {
+        if (!hasClass(el, name)) {
+            el.className += (el.className ? ' ' : '') +name;
+        }
+    }
+    function removeClass(el, name) {
+        if (hasClass(el, name)) {
+            el.className=el.className.replace(new RegExp('(\\s|^)'+name+'(\\s|$)'),' ').replace(/^\s+|\s+$/g, '');
+        }
+    }
+
+    var pageStates = [
+        'show-operatorselection',
+        'show-directsubscribe',
+        'show-doubleconfirmation',
+        'show-subscriptionpolling',
+        'show-numberentry',
+        'show-pinentry',
+        'show-mo',
+        'show-congrats',
+        'show-error'
+    ];
+    var container = document.getElementById('container');
+
+    window.state = function(string) {
+
+        if (string == null || string == '' || string == '?' || string == 'help') {
+            console.log(
+                "You can use the following command:\n\n" +
+                "- Operator Selection: \t\tstate('show-operatorselection')\n" +
+                "- Default: \t\t\t\t\tstate('show-directsubscribe')\n" +
+                "- Double Confirmation: \t\tstate('show-doubleconfirmation')\n" +
+                "- Subscription Polling: \tstate('show-subscriptionpolling')\n" +
+                "- Number Entry: \t\t\tstate('show-numberentry')\n" +
+                "- PIN Entry: \t\t\t\tstate('show-pinentry')\n" +
+                "- MO Message: \t\t\t\tstate('show-mo')\n" +
+                "- Congrats: \t\t\t\tstate('show-show-congrats')\n" +
+                "- Error: \t\t\t\t\tstate('show-error')"
+            );
+        }
+        else {
+            for(var i=0; i<pageStates.length; i++) {
+                removeClass(container, pageStates[i]);
+            }
+            addClass(container, string);
+        }
+    }
+
+}
