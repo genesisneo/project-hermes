7bdcade73ded0dabb5ad2d4aca2d5908041cc9a8
Neo Genesis <genesis_neo@live.com>
2017-06-27 10:10:27 +0400

    commit js template for designer

---
+++ public/assets/js/script.js
@@ -0,0 +1,12 @@
+
+function AfterPageLoaded() {
+    // this function is called when the page is `fully` loaded or DOMContentLoaded.
+}
+
+function BeforeStateChange() {
+    // this function is called `before` the page will change it's states.
+}
+
+function OnStateChanged() {
+    // this function is called when the page is `currently` changing it's states.
+}
